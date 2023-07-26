08d2ed49c1be72b98a24e29a8674cf153810b3c6
iyasinsky <iyasinsky@users.noreply.github.com>
2023-07-26 13:45:39 +0000

    deploy: 31a2cda21f14e34699efe9f3e98208a051468517

--- assets/task1-64acc5d4.js
+++ assets/task1-64acc5d4.js
@@ -1,1 +1,2 @@
 import"./main-a3e04fc6.js";const e=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");let r=null;e.addEventListener("click",t=>{t.target.disabled="true",o.disabled="",r=setInterval(()=>{const a=d(),n=document.body.style;n.backgroundColor=`${a}`},1e3)});o.addEventListener("click",t=>{t.target.disabled="true",e.disabled="",clearInterval(r)});function d(){return`#${Math.floor(Math.random()*16777215).toString(16).padStart(6,0)}`}
+//# sourceMappingURL=task1-64acc5d4.js.map
